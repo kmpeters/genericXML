908979cccc24c3478a8d559559b28459aa3ecfaf
Kevin Peterson <kmpeters@anl.gov>
2011-06-15 16:17:03 -0500

    Added example of generic command interpreter (snapshot of xa.py)

---
+++ genericInterpreter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+
+def showHelp(args):
+	#print "showHelp()"
+	print "showHelp(", args, ")"
+
+def printStuff(args):
+	#print "printStuff()"
+	print "printStuff(", args, ")"
+
+def listStuff(args):
+	#print "listStuff()"
+	print "listStuff(", args, ")"
+
+def correct(args):
+	#print "correct()"
+	print "correct(", args, ")"
+
+def main():
+	while ( True ):
+		try:
+			cmnd = raw_input(" > ")
+		except KeyboardInterrupt:
+			print ""
+			print "Exiting..."
+			break
+
+		if len(cmnd) > 0:
+			cmndKey = cmnd.split(" ")[0]
+			args = cmnd.split(" ")[1:]
+			print cmnd, cmndKey, commands.keys()
+			if cmndKey in commands.keys():
+				print commands[cmndKey]
+				commands[cmndKey](args)
+
+if __name__ == "__main__":
+
+	commands = { 
+			 "help":  showHelp,
+			    "h":  showHelp,  
+			"print":  printStuff,
+			    "p":  printStuff,
+			 "list":  listStuff,
+			    "l":  listStuff,
+			    "c":  correct
+		   }
+
+	main()
+
