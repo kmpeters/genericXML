bf87388591948075e26c6b6428737a114b16ff4f
Kevin Peterson <kmpeters@anl.gov>
2011-06-22 09:54:19 -0500

    Copied the OO versions to the main versions (should have used a git branch)

--- kmpXML.py
+++ kmpXML.py
@@ -5,68 +5,102 @@
 
 import xml.etree.ElementTree as etree
 import shutil
+import os.path
 
-def recursiveAddElem(labels, entries, elem, level=0):
-	for i in range(len(labels)):
-		if type(labels[i]) is str:
-			e = etree.SubElement(elem, labels[i])
-			e.text = entries[i]
-		if type(labels[i]) is dict:
-			key = labels[i].keys()[0]
-			e = etree.SubElement(elem, key)
-			recursiveAddElem(labels[i][key], entries[i], e, level+1)
+class kmpXML:
+	def __init__(self, filename, xmlRoot):
+		self.run = True
+		self.dirty = False
+		self.filename = filename
+		# Split filename into dir and name
+		self.directory = os.path.split(filename)[0]
+		self.logfilename = os.path.split(filename)[1]
+		# Handle empty directories when file is in cwd
+		if self.directory == '':
+			self.directory = "."
 
-	if level == 0:
-		pass
+		# Check to see if the file exists
+		if not os.path.isfile(filename):
+			print filename, "DOES NOT EXIST!"
 
-def appendEntry(root, entryStr):
-	length = len(root) + 1
-	entry = etree.SubElement(root, entryStr, index="%i" % length)
-	return entry
+			# Ask the user if the file should be created
+			decision = raw_input("Would you like to create it? (y/n) ")
+			if decision in ("Yes", "yes", "Y", "y"):
+				# Create the file
+				print "Creating %s" % filename
+				self.createEmptyXML(filename, xmlRoot)
+			else:
+				self.run = False
 
-def addEntry(root, xmlEntry, labels, entries):
-	# Add an entry to the root
-	entry = appendEntry(root, xmlEntry)
-	# Populate the entry with the users input
-	recursiveAddElem(labels, entries, entry)
+		if self.run == True:
+			# Read the file
+			print "Reading %s" % filename
+			self.tree, self.root = self.readXML(filename)
 
-def indent(elem, level=0):
-	i = "\n" + level*"  "
-	if len(elem):
-		if not elem.text or not elem.text.strip():
-			elem.text = i + "  "
-		if not elem.tail or not elem.tail.strip():
-			elem.tail = i
-		for elem in elem:
-			indent(elem, level+1)
-		if not elem.tail or not elem.tail.strip():
-			elem.tail = i
-	else:
-		if level and (not elem.tail or not elem.tail.strip()):
-			elem.tail = i
+	def recursiveAddElem(self, labels, entries, elem, level=0):
+		for i in range(len(labels)):
+			if type(labels[i]) is str:
+				e = etree.SubElement(elem, labels[i])
+				e.text = entries[i]
+			if type(labels[i]) is dict:
+				key = labels[i].keys()[0]
+				e = etree.SubElement(elem, key)
+				self.recursiveAddElem(labels[i][key], entries[i], e, level+1)
 
-def createEmptyXML(filename, xmlRoot):
-	tree = etree.ElementTree()
-	root = etree.Element(xmlRoot)
-	# Add a comment so an empty xml file can be properly read
-	comment = etree.Comment("This is an empty file")
-	root.append(comment)
-	indent(root)
-	tree._setroot(root)
-	tree.write(filename)
+		if level == 0:
+			pass
 
-def readXML(filename):
-	# Protection is done at a higher level
-	tree = etree.parse(filename)
-	root = tree.getroot()
-	return tree, root
+	def appendEntry(self, root, entryStr):
+		length = len(root) + 1
+		entry = etree.SubElement(root, entryStr, index="%i" % length)
+		return entry
 
-def saveXML(tree, root, filename):
-	# Move current file to backup
-	newfilename = "%s.bup" % filename
-	shutil.move(filename, newfilename)
-	# Make tree presentable
-	indent(root)
-	# Write file
-	tree.write(filename)
-	# How to handle dirty flag?
+	def addEntry(self, xmlEntry, labels, entries):
+		# Add an entry to the root
+		entry = self.appendEntry(self.root, xmlEntry)
+		# Populate the entry with the users input
+		self.recursiveAddElem(labels, entries, entry)
+		# Set the dirty flag
+		self.dirty = True
+
+	def indent(self, elem, level=0):
+		i = "\n" + level*"  "
+		if len(elem):
+			if not elem.text or not elem.text.strip():
+				elem.text = i + "  "
+			if not elem.tail or not elem.tail.strip():
+				elem.tail = i
+			for elem in elem:
+				self.indent(elem, level+1)
+			if not elem.tail or not elem.tail.strip():
+				elem.tail = i
+		else:
+			if level and (not elem.tail or not elem.tail.strip()):
+				elem.tail = i
+
+	def createEmptyXML(self, filename, xmlRoot):
+		tree = etree.ElementTree()
+		root = etree.Element(xmlRoot)
+		# Add a comment so an empty xml file can be properly read
+		comment = etree.Comment("This is an empty file")
+		root.append(comment)
+		self.indent(root)
+		tree._setroot(root)
+		tree.write(filename)
+
+	def readXML(self, filename):
+		# Protection is done at a higher level
+		tree = etree.parse(filename)
+		root = tree.getroot()
+		return tree, root
+
+	def saveXML(self):
+		# Move current file to backup
+		newfilename = "%s.bup" % self.filename
+		shutil.move(self.filename, newfilename)
+		# Make tree presentable
+		self.indent(self.root)
+		# Write file
+		self.tree.write(self.filename)
+		# Clear the dirty flag
+		self.dirty = False
--- xa.py
+++ xa.py
@@ -1,218 +1,194 @@
 #!/usr/bin/env python
 
-### Functions to customize (and maybe move to a different file)
-def prePromptHook(arg):
-	return
+class genXml:
+	def __init__(self):
+		self.userDefinitions()
 
-# Begin dummy functions
-def dummy(*args):
-	return True
+		# Open the log file
+		self.xmlLog = self.openLogFile()
 
-def showHelp(*args):
-	#print "showHelp()"
-	print "showHelp(", args, ")"
-	return True
+	def userDefinitions(self):
+		self.commands = { 
+				 "help": self.dummy,
+				    "h": self.dummy,  
+				"print": self.printStuff,
+				    "p": self.printStuff,
+				 "list": self.dummy,
+				    "l": self.dummy,
+				    "c": self.dummy,
+				  "add": self.addData,
+				    "a": self.addData,
+				 "quit": self.quit,
+				 "save": self.save,
+				    "s": self.save
+			   }
 
-def listStuff(*args):
-	#print "listStuff()"
-	print "listStuff(", args, ")"
-	return True
+		self.xmlFilename = "generic_log.xml"
+		self.xmlRoot = "log"
+		self.xmlEntry = "entry"
 
-def correct(*args):
-	#print "correct()"
-	print "correct(", args, ")"
-	return True
-# End dummy functions
+		self.xmlEntryDef = [
+				"date",
+				"author",
+				{"remark":
+				[
+					"title",
+					"subject",
+					{"subremark":
+					[
+						"subtitle",
+						"subsubject"
+					]},
+					"index",
+					"effort"
+				]},
+				"note"
+			]
 
-def printStuff(*args):
-	#print "printStuff()"
-	print "printStuff(", args, ")"
-	print xmlEntryDef
-	recursivePrintList(xmlEntryDef)
+	### Functions to customize (and maybe move to a different file)
+	def prePromptHook(self, arg):
+		return
+
+	# Begin dummy functions
+	def dummy(self, *args):
+		return True
+
+	def showHelp(self, *args):
+		#print "showHelp()"
+		print "showHelp(", args, ")"
+		return True
+
+	def listStuff(self, *args):
+		#print "listStuff()"
+		print "listStuff(", args, ")"
+		return True
+
+	def correct(self, *args):
+		#print "correct()"
+		print "correct(", args, ")"
+		return True
+	# End dummy functions
+
+	def printStuff(self, *args):
+		#print "printStuff()"
+		print "printStuff(", args, ")"
+		print self.xmlEntryDef
+		self.recursivePrintList(self.xmlEntryDef)
 	
-	return True
+		return True
 
-def recursivePrintList(arg):
-	for i in arg:
-		if type(i) is str:
-			print i, "is a string"
-		elif type(i) is list:
-			print i, "is a list"
-			recursivePrintList(i)
-		elif type(i) is dict:
-			# by design dictionary will only have one key
-			key = i.keys()[0]
-			print i, "is a dictionary"
-			recursivePrintList(i[key])
+	def recursivePrintList(self, arg):
+		for i in arg:
+			if type(i) is str:
+				print i, "is a string"
+			elif type(i) is list:
+				print i, "is a list"
+				self.recursivePrintList(i)
+			elif type(i) is dict:
+				# by design dictionary will only have one key
+				key = i.keys()[0]
+				print i, "is a dictionary"
+				self.recursivePrintList(i[key])
+			else:
+				print type(i)
+
+	def quit(self, *args):
+		print "Exiting..."
+		return False
+
+	def save(self, *args):
+		self.xmlLog.saveXML()
+		return True
+
+	def recursivePromptEntry(self, labels, array, level=0):
+		for i in range(len(labels)):
+			if type(labels[i]) is str:
+				# Allow for printing auto-complete suggestions or customizing prompt
+				self.prePromptHook(labels[i])
+				# Generic promptStr
+				promptStr = "  " * level + "%s: " % labels[i]
+				# Will it be possible to validate response?
+				response = raw_input(promptStr)
+				array.append(response)
+			elif type(labels[i]) is list:
+				print "OMG!!! THIS IS A LIST!!!"
+				array.append([])
+				self.recursivePromptEntry(labels[i], array[i], level+1)
+			elif type(labels[i]) is dict:
+				array.append([])
+				# by design dictionary will only have one key
+				key = labels[i].keys()[0]
+				print "  " * level + "[%s]" % key
+				self.recursivePromptEntry(labels[i][key], array[i], level+1)
+	
+		if level == 0:
+			#!print array
+			pass
+
+	def recursiveAddEntry(self, labels, entries, level=0):
+		for i in range(len(labels)):
+			if type(labels[i]) is str:
+				print labels[i], "->",entries[i]
+			if type(labels[i]) is dict:
+				# by design dictionary will only have one key
+				key = labels[i].keys()[0]
+				print "*%s" % key
+				self.recursiveAddEntry(labels[i][key], entries[i], level+1)
+
+	def getUserInput(self):
+		# At this point in time there is no helpful printing of existing entries or autocomplete
+		userInput = []
+		self.recursivePromptEntry(self.xmlEntryDef, userInput)
+		return userInput[:]
+
+	def addData(self, *args):
+		print "addData(", args, ")"
+	
+		print
+		userEntries = self.getUserInput()
+		print
+
+		# This code could be cleaner if xml definition stuff was moved
+		self.xmlLog.addEntry(self.xmlEntry, self.xmlEntryDef, userEntries)
+	
+		return True
+
+	def openLogFile(self):
+		# Allow the user to specify an xml file when running the script
+		if len(sys.argv) > 1:
+			filename = sys.argv[1]
 		else:
-			print type(i)
+			filename = "%s/%s" % (os.getcwd(), self.xmlFilename)
+			# Alternately could use the script location (sys.argv[0]), but that seems like a bad decision
 
-def quit(*args):
-	print "Exiting..."
-	return False
+		#
+		return kmpXMLoo.kmpXML(filename, self.xmlRoot) 
 
-def save(*args):
-	global filename
-	#!print tree, log, filename
-	kmpXML.saveXML(tree, log, filename)
-	return True
+	def main(self):
+		run = self.xmlLog.run
 
-def recursivePromptEntry(labels, array, level=0):
-	for i in range(len(labels)):
-		if type(labels[i]) is str:
-			# Allow for printing auto-complete suggestions or customizing prompt
-			prePromptHook(labels[i])
-			# Generic promptStr
-			promptStr = "  " * level + "%s: " % labels[i]
-			# Will it be possible to validate response?
-			response = raw_input(promptStr)
-			array.append(response)
-		elif type(labels[i]) is list:
-			print "OMG!!! THIS IS A LIST!!!"
-			array.append([])
-			recursivePromptEntry(labels[i], array[i], level+1)
-		elif type(labels[i]) is dict:
-			array.append([])
-			# by design dictionary will only have one key
-			key = labels[i].keys()[0]
-			print "  " * level + "[%s]" % key
-			recursivePromptEntry(labels[i][key], array[i], level+1)
-	
-	if level == 0:
-		#!print array
-		pass
+		# Enter command interpreter mode
+		while ( run ):
+			try:
+				cmnd = raw_input(" > ")
+			except KeyboardInterrupt:
+				print ""
+				print "Exiting..."
+				break
 
-def recursiveAddEntry(labels, entries, level=0):
-	for i in range(len(labels)):
-		if type(labels[i]) is str:
-			print labels[i], "->",entries[i]
-		if type(labels[i]) is dict:
-			# by design dictionary will only have one key
-			key = labels[i].keys()[0]
-			print "*%s" % key
-			recursiveAddEntry(labels[i][key], entries[i], level+1)
-
-def getUserInput():
-	# At this point in time there is no helpful printing of existing entries or autocomplete
-	userInput = []
-	recursivePromptEntry(xmlEntryDef, userInput)
-	return userInput[:]
-
-def addData(*args):
-	print "addData(", args, ")"
-	
-	print
-	userEntries = getUserInput()
-	print
-
-	# This code could be cleaner if xml definition stuff was moved
-	kmpXML.addEntry(log, xmlEntry, xmlEntryDef, userEntries)
-	
-	return True
-
-def openLogFile():
-	# Allow the user to specify an xml file when running the script
-	if len(sys.argv) > 1:
-		filename = sys.argv[1]
-	else:
-		filename = "%s/%s" % (os.getcwd(), xmlFilename)
-		# Alternately could use the script location (sys.argv[0]), but that seems like a bad decision
-
-	# turn the filename into something useful (why was this needed in other code?)
-	directory = os.path.split(filename)[0]
-	logfilename = os.path.split(filename)[1]
-
-	# Handle empty directories when file is in current dir
-	if directory == '':
-		directory = "."
-	#print "%s/%s" % (directory, logfilename)
-
-	# Check to see if the file exists
-	if not os.path.isfile(filename):
-		print filename, "DOES NOT EXIST!"
-
-		# Ask the user if the file should be created
-		decision = raw_input("Would you like to create it? (y/n) ")
-		if decision in ("Yes", "yes", "Y", "y"):
-			# Create the file
-			print "Creating %s" % filename
-			kmpXML.createEmptyXML(filename, xmlRoot)
-		else:
-			print "Exiting"
-			return (False, None, None)
-
-	# Read the file
-	print "Reading %s" % filename
-	tree, log = kmpXML.readXML(filename)
-	return (True, tree, log, filename)
-
-def main():
-	global tree, log, filename
-	# Open the log file
-	run, tree, log, filename = openLogFile()
-
-	# Enter command interpreter mode
-	while ( run ):
-		try:
-			cmnd = raw_input(" > ")
-		except KeyboardInterrupt:
-			print ""
-			print "Exiting..."
-			break
-
-		if len(cmnd) > 0:
-			cmndKey = cmnd.split(" ")[0]
-			args = cmnd.split(" ")[1:]
-			#!print cmnd, cmndKey, commands.keys()
-			if cmndKey in commands.keys():
-				#!print commands[cmndKey]
-				run = commands[cmndKey](*args)
+			if len(cmnd) > 0:
+				cmndKey = cmnd.split(" ")[0]
+				args = cmnd.split(" ")[1:]
+				#!print cmnd, cmndKey, commands.keys()
+				if cmndKey in self.commands.keys():
+					#!print commands[cmndKey]
+					run = self.commands[cmndKey](*args)
 
 if __name__ == "__main__":
 	import sys
 	import os
 	import kmpXML
 
-	commands = { 
-			 "help":  dummy,
-			    "h":  dummy,  
-			"print":  printStuff,
-			    "p":  printStuff,
-			 "list":  dummy,
-			    "l":  dummy,
-			    "c":  dummy,
-			  "add":  addData,
-			    "a":  addData,
-			 "quit":  quit,
-			 "save":  save,
-			    "s":  save
-		   }
-
-	xmlFilename = "generic_log.xml"
-	xmlRoot = "log"
-	xmlEntry = "entry"
-
-	xmlEntryDef = [
-			"date",
-			"author",
-			{"remark":
-			[
-				"title",
-				"subject",
-				{"subremark":
-				[
-					"subtitle",
-					"subsubject"
-				]},
-				"index",
-				"effort"
-			]},
-			"note"
-		]
-
-	filename = ""
-	tree = None
-	log = None
-				
-	main()
+	ui = genXml()			
+	ui.main()
 
